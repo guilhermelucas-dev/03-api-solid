cbb65f6bab107a5c2e5f18017dfa483f9d2b14ae
guilherme <guilhermedesouzalucas@gmail.com>
2024-11-21 15:50:24 -0300

    add factory

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify"
 import { z } from "zod";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate.use-case";
 
 export async function Authenticate(request: FastifyRequest, reply: FastifyReply) {
   const authenticateBodySchema = z.object({
@@ -14,8 +13,7 @@ export async function Authenticate(request: FastifyRequest, reply: FastifyReply)
 
   try {
 
-    const usersRepository = new PrismaUsersRepository();
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+    const authenticateUseCase = makeAuthenticateUseCase();
 
     await authenticateUseCase.execute({
       email,
